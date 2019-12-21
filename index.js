233bdd26a4cb5fd4bca8ac4fbbab4fe34f8a354e
TreverG123 <help.tgumbs1@gmail.com>
2019-12-21 16:33:30 -0500

    upload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const config = require("./config.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = config.prefix;
+const {Client, RichEmbed} = require('discord.js');
+const Enmap = require('enmap');
+
 
 client.on("ready", () => {
 
@@ -11,41 +13,30 @@ client.on("ready", () => {
 
 });
 
-client.on("message", message => {
+client.config = config;
 
-let command = message.content.slice(prefix.length).split(" ")[0];
+fs.readdir("./events/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+      const event = require(`./events/${file}`);
+      let eventName = file.split(".")[0];
+      client.on(eventName, event.bind(null, client));
+    });
+  });
+  
 
-let args = message.content.slice(1).split(" ");
+client.commands = new Enmap();
 
-if (message.author.bot) return;
-
-if (!message.guild) return;
-
-if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(":( You fool; You have no permission to control me.");
-
-if (command === "ping") {
-
-    message.channel.send(`Pong! \`${client.pings.length}ms\``);
-
-
-}
-
-
-if (command === "rmchannel") {
-
-    if (!message.mentions.channels.first()) return message.channel.send("Please mention a channel to delete.");
-
-    if (message.mentions.channels.first() === message.channel) return message.channel.send("Nah bro, lets not delete the channel we're in.");
-
-    let channel = message.mentions.channels.first();
-
-    channel.delete();
-
-    message.channel.send(`**${message.author.tag}**, The channel **${channel.name}** was deleted.`);
-
-
-}
-
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Loaded ${commandName}`);
+    client.commands.set(commandName, props);
+  });
 });
+
 
 client.login(config.token);
