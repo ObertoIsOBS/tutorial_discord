261c2045ea5afa82ae632770ecdbaf0705736041
Jonfirexbox <66625200+Jonfirexbox@users.noreply.github.com>
2021-03-25 17:08:33 -0400

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,6 +1,6 @@
-Discord = require("discord.js");
+const Discord = require("discord.js");
 module.exports.run = (client, message, args) => {
 
-message.channel.send(`This bot was created by OBS (info command made by Jonfirexbox) You can also make this bot your self by going to https://github.com/ObertoIsOBS/tutorial_discord/')
+message.channel.send(`This bot was created by OBS (info command made by Jonfirexbox) You can also make this bot your self by going to https://github.com/ObertoIsOBS/tutorial_discord/`)
 
 }
